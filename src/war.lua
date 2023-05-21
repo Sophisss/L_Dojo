b701369c8ac3ae06389390cf97a6aa37c57a3c10
Sofia <scattolinisofia26@gmail.com>
2023-05-21 12:28:11 +0200

    [MODIFY] function to comparison two sempai.

--- src/war.lua
+++ src/war.lua
@@ -3,14 +3,12 @@ local init_function = require("init")
 
 
 --Funzione che rimuove un sempai dalla scacchiera
-local function deleteSempai (board, sempai, N)
+local function deleteSempai (board, sempai)
 
     local newBoard = init_function.clone(board)
 
     local x = sempai.posizione.x
     local y = sempai.posizione.y
-
-    init_function.print(newBoard, N)
 
     newBoard[x][y] = '-'
 
@@ -33,33 +31,37 @@ end
 
 
 --Funzione che permette di comparare due sempai quando si trovano su due caselle vicine
-local function comparison(board, sempaiToCompare, N)
+local function comparison(board)
 
     local newBoard = init_function.clone(board)
 
     local position_sempai = sempai_function.searchSempai(newBoard)
 
     for i=1, #position_sempai do
-        local sempai = position_sempai[i]
-        local x, y = sempai.posizione.x, sempai.posizione.y
+        for j=2, #position_sempai do
+            local sempai1 = position_sempai[i]
+            local x, y = sempai1.posizione.x, sempai1.posizione.y
+
+            local sempai2 = position_sempai[j]
+            local x2, y2 = sempai2.posizione.x, sempai2.posizione.y
 
 
-        if (x == sempaiToCompare.posizione.x +1 and y == sempaiToCompare.posizione.y) or
-                (x== sempaiToCompare.posizione.x -1 and y == sempaiToCompare.posizione.y) or
-                (y == sempaiToCompare.posizione.y +1 and x == sempaiToCompare.posizione.x) or
-                (y == sempaiToCompare.posizione.y -1 and x == sempaiToCompare.posizione.x) then
+            if (x == x2 +1 and y == y2) or
+                    (x== x2 -1 and y == y2) or
+                    (y == y2 +1 and x == x2) or
+                    (y == y2 -1 and x == x2) then
 
-            local tot_point_sempai1 = sempai_function.sum(sempaiToCompare)
-            local tot_point_sempai2 = sempai_function.sum(sempai)
+                local tot_point_sempai1 = sempai_function.sum(sempai1)
+                local tot_point_sempai2 = sempai_function.sum(sempai2)
 
-            if tot_point_sempai1 >  tot_point_sempai2 then
-                newBoard = deleteSempai(newBoard, sempai, N)
-            elseif tot_point_sempai1 <  tot_point_sempai2 then
-                newBoard = deleteSempai(newBoard, sempaiToCompare, N)
-            elseif tot_point_sempai1 ==  tot_point_sempai2 then
-                local minPrioritySempai = calculatePriority(sempaiToCompare, sempai)
-                newBoard = deleteSempai(newBoard, minPrioritySempai, N)
-
+                if tot_point_sempai1 >  tot_point_sempai2 then
+                    newBoard = deleteSempai(newBoard, sempai2)
+                elseif tot_point_sempai1 <  tot_point_sempai2 then
+                    newBoard = deleteSempai(newBoard, sempai1)
+                elseif tot_point_sempai1 ==  tot_point_sempai2 then
+                    local minPrioritySempai = calculatePriority(sempai1, sempai2)
+                    newBoard = deleteSempai(newBoard, minPrioritySempai)
+                end
             end
         end
     end
@@ -71,6 +73,7 @@ end
 
 local W = {
     deleteSempai = deleteSempai,
+    calculatePriority = calculatePriority,
     comparison = comparison
 }
 
